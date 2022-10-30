2b4108b7bc2832df67e7be8ae7c6c47e58367aa4
Deepak Tiwari <82175716+deepaktiwari09@users.noreply.github.com>
2022-10-30 22:49:49 +0530

    vercal buld error fix

--- store/index.ts
+++ store/index.ts
@@ -2,6 +2,6 @@ import create from 'zustand'
 
 const usePath = create((set) => ({
     currentPathName: '',
-    updatePath: (path: string) => set((state) => state.currentPathName = path)
+    // updatePath: (path: string) => set((state) => state.currentPathName = path)
 }))
 export default usePath
