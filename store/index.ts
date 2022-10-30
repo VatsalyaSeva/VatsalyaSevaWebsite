d892f7e6ec8fdf62badac3c796e43bc87411b540
Deepak Tiwari <82175716+deepaktiwari09@users.noreply.github.com>
2022-10-30 22:35:00 +0530

    Home Page Updated

---
+++ store/index.ts
@@ -0,0 +1,7 @@
+import create from 'zustand'
+
+const usePath = create((set) => ({
+    currentPathName: '',
+    updatePath: (path: string) => set((state) => state.currentPathName = path)
+}))
+export default usePath
