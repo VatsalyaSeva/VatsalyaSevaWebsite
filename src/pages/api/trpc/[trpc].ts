904f96e219a79ce1df3d1e6ca672867c96153050
deepak tiwari <deepaktiwari3020@gmail.com>
2023-02-20 15:15:02 +0530

    update 13

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,9 +3,10 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "../../../env.mjs";
 import { createTRPCContext } from "../../../server/api/trpc";
 import { appRouter } from "../../../server/api/root";
+import { NextApiRequest, NextApiResponse } from "next";
 
 // export API handler
-export default createNextApiHandler({
+const nextApiHandler =  createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
@@ -17,3 +18,30 @@ export default createNextApiHandler({
         }
       : undefined,
 });
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  // Modify `req` and `res` objects here
+  // In this case, we are enabling CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  // If you need to make authenticated CORS calls then
+  // remove what is above and uncomment the below code
+  // Allow-Origin has to be set to the requesting domain that you want to send the credentials back to
+  // res.setHeader('Access-Control-Allow-Origin', 'http://example:6006');
+  // res.setHeader('Access-Control-Request-Method', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  // res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  // res.setHeader('Referrer-Policy', 'no-referrer');
+  // res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    return res.end();
+  }
+  // pass the (modified) req/res to the handler
+  return nextApiHandler(req, res);
+}
