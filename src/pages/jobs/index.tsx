870f6dbb26aaf1099784f5d252991cd52112191a
deepak tiwari <deepaktiwari3020@gmail.com>
2023-04-05 22:00:26 +0530

    job application end date fix

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { api } from "../../utils/api";
 import { Box, Heading,Grid, GridItem, Stack,Text, Flex, Center } from "@chakra-ui/react";
 import ReactHtmlParser from 'react-html-parser'; 
+import dayjs from "dayjs";
 
 
 export default function Jobs() {
@@ -37,61 +38,66 @@ export default function Jobs() {
         {jobList.map((item, index) => {
           return (
             <GridItem w='100%' h='100%' bg={'gray.50'} px='5' py='5'rounded={'xl'} boxShadow={'lg'} >
-                <Stack spacing={'5'}>
-                    <div>
-                        <p className="text-lg font-medium">{item.vacancyName}</p>
-                        <p className="pb-2 text-sm">
-                        {ReactHtmlParser(item.vacancyDescription.slice(0, 90))}
-                        </p>
-                        <div className="flex lg:flex-row sm:flex-column items-center justify-between border-t-2 border-t-red-600 pt-3">
-                        <p className="text-sm font-medium text-gray-700">
-                            Job Location
-                        </p>
-                        <p className="text-sm">{item.location}</p>
-                        </div>
-                        <div className="flex lg:flex-row sm:flex-column items-center justify-between">
-                        <p className="text-sm font-medium text-gray-700">
-                            Accepted Salary
-                        </p>
-                        <p className="text-sm">{item.salary} ₹</p>
-                        </div>
-                        <div className="flex flex-row items-center justify-between">
-                        <p className="text-sm font-medium text-gray-700">
-                            Total Posts
-                        </p>
-                        <p className="text-sm">{item.jobCount}</p>
-                        </div>
-                        <div className="flex flex-row items-center justify-between">
-                        <p className="text-sm font-medium text-gray-700">
-                            Application Last Date
-                        </p>
-                        <p className="text-sm">
-                            {new Date(item.lastSubmissionDate).toDateString()}
-                        </p>
-                        </div>
-                        <div className="flex flex-row items-center justify-between">
-                        <p className="text-sm font-medium text-gray-700">
-                            Interview Date
-                        </p>
-                        <p className="text-sm">
-                            {new Date(item.interviewDate).toDateString()}
-                        </p>
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center justify-end space-x-3">
-                        <p className="text-sm font-medium text-gray-700">
-                        Application Fees : {item.fees} ₹
-                        </p>
-                        <button
-                        className="rounded-lg bg-red-600 px-5 py-1 text-white"
-                        onClick={() => {
-                            router.push(`jobs/${item.id}`);
-                        }}
-                        >
-                        Apply
-                        </button>
-                    </div>
-                </Stack>
+                
+                  <div className="flex flex-col h-[100%] justify-between">
+                      <div>
+                          <p className="text-lg font-medium">{item.vacancyName}</p>
+                          <p className="pb-2 text-sm">
+                          {ReactHtmlParser(item.vacancyDescription.slice(0, 90))}
+                          </p>
+                          
+                          <div className="flex lg:flex-row sm:flex-column items-center justify-between">
+                          <p className="text-sm font-medium text-gray-700">
+                          Expected Salary
+                          </p>
+                          <p className="text-sm">{item.salary} ₹</p>
+                          </div>
+                          <div className="flex flex-row items-center justify-between">
+                          <p className="text-sm font-medium text-gray-700">
+                              Total Posts
+                          </p>
+                          <p className="text-sm">{item.jobCount}</p>
+                          </div>
+                          <div className="flex flex-row items-center justify-between">
+                          <p className="text-sm font-medium text-gray-700">
+                              Application Last Date
+                          </p>
+                          <p className="text-sm">
+                              {new Date(item.lastSubmissionDate).toDateString()}
+                          </p>
+                          </div>
+                          {dayjs(item.interviewDate).isValid() &&<div className="flex flex-row items-center justify-between">
+                          <p className="text-sm font-medium text-gray-700">
+                              Interview Date
+                          </p>
+                          <p className="text-sm">
+                              {new Date(item.interviewDate).toDateString()}
+                          </p>
+                          </div>}
+                          <div className="flex lg:flex-row sm:flex-column items-center justify-between border-t-2 border-t-red-600 pt-3 mt-2">
+                          <p className="text-sm font-medium text-gray-700">
+                              Job Locations
+                          </p>
+                          <p className="text-sm max-w-[200px]">{item.location}</p>
+                          </div>
+                      </div>
+                      {dayjs().isBefore(item.lastSubmissionDate) ? <div className="flex flex-row items-center justify-end space-x-3">
+                          <p className="text-sm font-medium text-gray-700">
+                          Application Fees : {item.fees} ₹
+                          </p>
+                          <button
+                          className="rounded-lg bg-red-600 px-5 py-1 text-white"
+                          onClick={() => {
+                              router.push(`jobs/${item.id}`);
+                          }}
+                          >
+                          Apply
+                          </button>
+                      </div>:<div className="flex flex-row justify-center items-end mt-3">
+                          <p className="text-gray-600">{`Application Form closed on ${dayjs(item.lastSubmissionDate).format('DD-MM-YYYY')}`}</p>
+                      </div>}
+                  </div>
+                
             </GridItem>
           );
         })}
